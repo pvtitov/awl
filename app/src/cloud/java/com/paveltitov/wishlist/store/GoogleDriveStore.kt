d8a3ce8f731e0c695b20d9ebdbc18ab8f02edbd8
Pavel Titov <ptitov.mailbox@gmail.com>
2022-10-23 14:51:34 +0300

    put google drive api key and shared directory url into local.properties

--- app/src/cloud/java/com/paveltitov/wishlist/store/GoogleDriveStore.kt
+++ app/src/cloud/java/com/paveltitov/wishlist/store/GoogleDriveStore.kt
@@ -1,11 +1,10 @@
 package com.paveltitov.wishlist.store
 
-import android.os.Handler
-import android.os.Looper
+import com.paveltitov.wishlist.BuildConfig
 import com.paveltitov.wishlist.data.Person
 import com.paveltitov.wishlist.data.Wish
 
-class MyWishListRuStore(private val httpClient: HttpClient) : Store {
+class GoogleDriveStore(private val httpClient: HttpClient) : Store {
     override fun register(
         login: String,
         password: String,
@@ -21,27 +20,7 @@ class MyWishListRuStore(private val httpClient: HttpClient) : Store {
         onSuccess: () -> Unit,
         onError: (message: String) -> Unit
     ) {
-        val mainHandler = Handler(Looper.getMainLooper())
-        Thread {
-            val response = httpClient.login("pavel_titov", "just4E")
-            when (response) {
-                is HttpClient.Response.Success -> response.response
-                is HttpClient.Response.Error -> response.message
-            }.let { raw ->
-                val results = mutableListOf<String>()
-                raw
-                    .split("<table class='Virgin'><tr><td><h5><a href=")
-                    .drop(0)
-                    .forEach { it
-                        .substringAfter("\">")
-                        .substringBefore("</a>")
-                    }
-
-                mainHandler.post {
-                    onError(results.toString())
-                }
-            }
-        }.start()
+        BuildConfig.DIR
     }
 
     override fun getMe(onSuccess: (me: Person) -> Unit, onError: (message: String) -> Unit) {
